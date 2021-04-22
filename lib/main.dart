2c0a98a857dbd84a3a801ce33cca7bc34f055e44
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2021-04-22 13:09:25 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -36,12 +36,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            
             Text(''),
-
-
-
-
-
 
           ],
         ),
